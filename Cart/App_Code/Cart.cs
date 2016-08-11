edc6bb089ec369ebcc03d4615776de609c0508ab
Juul94 <juul.94@hotmail.com>
2016-08-11 08:25:00 +0200

    Same as the last one

---
+++ Cart/App_Code/Cart.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Excep
+{
+    [Serializable]
+
+    public class Cart
+    {
+
+        #region Construntor
+
+            public Cart()
+            {
+                
+                this.items = GrabCart();
+            }
+
+        #endregion
+
+        #region method
+
+            public List<CartProduct> Items { get { return this.items; } }
+
+            public List<CartProduct> items;
+
+            private List<CartProduct> GrabCart()
+            {
+                List<CartProduct> cart = new List<CartProduct>();
+
+                if (HttpContext.Current.Session["Cart"] == null)
+                {
+                    HttpContext.Current.Session.Add("Cart", cart);
+                }
+
+                cart = (List<CartProduct>)HttpContext.Current.Session["Cart"];
+
+                return cart;
+            }
+
+            public void AddToCart(int id, string name, decimal price, int amount)
+            {
+                CartProduct product = findProduct(id);
+                
+                if(product.Id != 0)
+                {
+                    product.Amount += amount;
+                }
+
+                else
+                {
+                    this.items.Add(new CartProduct(id, name, price, amount));
+                }
+            }
+
+            public void SetAmountOnProduct(int id, int newAmount)
+
+            {
+                CartProduct item = findProduct(id);
+                item.Amount = newAmount;
+            }
+
+            public void addAmountOnProduct(int id, int amountToAdd)
+            {
+                CartProduct item = findProduct(id);
+                item.Amount += amountToAdd;
+            }
+
+            public void reduceAmountOnProduct(int id, int amountToReduce)
+            {
+                CartProduct item = findProduct(id);
+                item.Amount -= amountToReduce;
+            }
+
+            public CartProduct findProduct(int id)
+            {
+                foreach (CartProduct product in this.items)
+                {
+                    if (product.Id == id)
+                    {
+                        return product;
+                    }
+                }
+
+                return new CartProduct();
+            }
+
+            public void removeProduct(int id)
+            {
+                CartProduct item = findProduct(id);
+
+                if(item.Id != 0)
+                {
+                    this.items.Remove(item);
+                }
+            }
+
+            public void removeallProducts()
+            {
+                if(HttpContext.Current.Session["Cart"] != null)
+                {
+                    HttpContext.Current.Session.Remove("Cart");
+                }
+                
+                this.items = new List<CartProduct>();
+            }
+
+
+         #endregion
+    }
+}
---
+++ Cart/CartView.ascx
@@ -0,0 +1,46 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="CartView.ascx.cs" Inherits="CartView" %>
+
+<asp:Repeater ID="Repeater_CartView" runat="server" OnItemCommand="Repeater_CartView_ItemCommand">
+
+    <HeaderTemplate>
+        <table>
+            <thead>
+                <th>Navn</th>
+                <th>Pris</th>
+                <th>Antal</th>
+                <th>Eks. Moms</th>
+                <th>Moms</th>
+                <th>Inkl. Moms</th>
+            </thead>
+        <tbody>
+    </HeaderTemplate>
+
+    <ItemTemplate>    
+        <tr>
+            <th><%#Eval("Name")%></th>
+            <th><%#Eval("Price")%></th>
+            <th>
+                <asp:Button ID="Button_Minus" runat="server" Text="-" CommandArgument='<%#Eval("Id") %>' CommandName='minus'/>
+                    <%#Eval("Amount")%>
+                <asp:Button ID="Button_Plus" runat="server" Text="+" CommandArgument='<%#Eval("Id") %>' CommandName='plus'/>
+            </th>
+            <th><%#Eval("TotalPrice")%></th>
+            <th><%#Eval("Vat")%></th>
+            <th><%#Eval("InklMoms")%></th>
+            <th>
+                <asp:Button ID="Button_removeProduct" runat="server" Text="Slet" CommandName="delete" CommandArgument='<%#Eval("id")%>'/>
+            </th>
+        </tr>
+    </ItemTemplate>
+
+    <FooterTemplate>
+        <tr>
+            <td colspan="5">
+            </td>
+        </tr>
+            </tbody>
+        </table>
+    </FooterTemplate>
+</asp:Repeater>
+
+    <asp:Button ID="Button_EmptyCart" runat="server" Text="Tøm Indkøbskurv" OnClick="Button_EmptyCart_Click" />
